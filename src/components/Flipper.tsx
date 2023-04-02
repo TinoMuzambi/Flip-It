b42fd86dc4464a7812b3ac48fcec703bc1f1e2ba
Tino Muzambi <tinomuzambi@gmail.com>
2023-04-02 16:34:52 +0200

    Adds flipping functionality.

--- src/components/Flipper.tsx
+++ src/components/Flipper.tsx
@@ -1,23 +1,28 @@
 import Image from "next/image";
+import { useState } from "react";
 import type { RouterOutputs } from "~/utils/api";
 
 type FlipperWithUser = RouterOutputs["flippers"]["getAll"][number];
 
 const Flipper = (props: FlipperWithUser) => {
+  const [flipped, setFlipped] = useState(false);
+
   const { flipper, author } = props;
+
   return (
     <div
-      className="relative flex min-h-[10rem] items-center justify-center rounded-md bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 p-4"
+      className="relative flex min-h-[10rem] cursor-pointer items-center justify-center rounded-md bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 p-4"
       key={flipper.id}
+      onClick={() => setFlipped((prev) => !prev)}
     >
       <Image
         src={author.profileImageUrl}
         alt={`${author.username || ""}'s profile picture`}
         width={24}
         height={24}
-        className="absolute left-2 top-2 rounded-full object-cover"
+        className="absolute left-2 top-2 rounded-full object-cover "
       />
-      {flipper.question}
+      {flipped ? flipper.answer : flipper.question}
     </div>
   );
 };
